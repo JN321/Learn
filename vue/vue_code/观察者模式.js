b6ad55b5c50201492a4f69b7e4616b41e539cfad
zhouwen <zhouwen427@163.com>
2022-05-31 07:41:31 +0800

    fix: 观察者模式

---
+++ vue/vue_code/观察者模式.js
@@ -0,0 +1,32 @@
+class Subject {
+  constructor(name) {
+    this.name = name
+    this.state = 'padding'
+    this.observers = []
+  }
+  setState(state) {
+    this.state = state
+    this.observers.forEach(observer => {
+      observer.update(state)
+    })
+  }
+  append(...observer) {
+    this.observers.push(...observer)
+  }
+}
+
+class Observer {
+  constructor(name) {
+    this.name = name
+  }
+  update(state) {
+    console.log(`观察者${this.name}看到的状态为:`, state);
+  }
+}
+
+const father = new Observer('爸爸')
+const mather = new Observer('妈妈')
+
+const baby = new Subject('宝宝')
+baby.append(father, mather)
+baby.setState('哭闹')
