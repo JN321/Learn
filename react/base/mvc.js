3eea9beefdcb981d93910d191b2a0213f03173c0
zhouwen <zhouwen427@163.com>
2022-06-07 22:59:41 +0800

    fix: mvc细节调整

--- react/base/mvc.js
+++ react/base/mvc.js
@@ -2,29 +2,24 @@
 (function () {
   // model
   var myapp = {}; // 创建这个应用对象
-
   myapp.Model = function () {
     var val = 0;
-
     this.add = function (v) {
       if (val < 100) val += v;
     };
-
     this.sub = function (v) {
       if (val > 0) val -= v;
     };
-
     this.getVal = function () {
       return val;
     };
-
     var self = this,
       views = [];
-
+    // 订阅
     this.register = function (view) {
       views.push(view);
     };
-
+    // 通知
     this.notify = function () {
       for (var i = 0; i < views.length; i++) {
         views[i].render(self);
@@ -34,45 +29,41 @@
 
   // view
   myapp.View = function (controller) {
-    var $num = $('#num'),
-      $incBtn = $('#increase'),
-      $decBtn = $('#decrease');
+    var $num = document.getElementById("num"),
+      $incBtn = document.getElementById("#increase"),
+      $decBtn = document.getElementById("#decrease");
 
     this.render = function (model) {
-      $num.text(model.getVal() + 'rmb');
+      $num.innerHTML = model.getVal() + "rmb";
     };
-
     /*  绑定事件  */
-    $incBtn.click(controller.increase);
-    $decBtn.click(controller.decrease);
+    $incBtn.addEventListener("click", controller.increase);
+    $decBtn.addEventListener("click", controller.decrease);
   };
 
   // controller
   myapp.Controller = function () {
     var model = null,
       view = null;
-
     this.init = function () {
       /* 初始化Model和View */
       model = new myapp.Model();
       view = new myapp.View(this);
-
       /* View向Model注册，当Model更新就会去通知View啦 */
       model.register(view);
       model.notify();
     };
-
     /* 让Model更新数值并通知View更新视图 */
     this.increase = function () {
       model.add(1);
       model.notify();
     };
-
     this.decrease = function () {
       model.sub(1);
       model.notify();
     };
   };
+
   var controller = new myapp.Controller();
   controller.init();
 })();
